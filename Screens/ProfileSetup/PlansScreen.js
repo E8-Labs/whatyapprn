3b0a3fbec8f9ea6f99c48cbb85fdc5d8c733bba3
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2024-11-07 02:10:06 +0500

    User subscriptions added

--- Screens/ProfileSetup/PlansScreen.js
+++ Screens/ProfileSetup/PlansScreen.js
@@ -1,128 +1,195 @@
-import { View, Text, SafeAreaView, TouchableOpacity, Image } from 'react-native'
-import React, { useState } from 'react'
-import { GlobalStyles } from '../../assets/styles/GlobalStyles'
-import { screenHeight, screenWidth } from '../../res/Constants'
-import { Colors } from '../../res/Colors'
-import { CustomFonts } from '../../assets/font/Fonts'
-import { ScreenNames } from '../../res/ScreenNames'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import React, { useState } from "react";
+import { GlobalStyles } from "../../assets/styles/GlobalStyles";
+import { screenHeight, screenWidth } from "../../res/Constants";
+import { Colors } from "../../res/Colors";
+import { CustomFonts } from "../../assets/font/Fonts";
+import { ScreenNames } from "../../res/ScreenNames";
 
-const PlansScreen = ({navigation}) => {
+const PlansScreen = ({ navigation }) => {
+  const [selectedPlan, setselectedPlan] = useState({
+    id: 1,
+    name: "YEARLY",
+    price: "$999/yr",
+    disc: "12 Months at $50/Month",
+  });
 
-    const [selectedPlan, setselectedPlan] = useState({
-        id: 1,
-        name: "YEARLY",
-        price: '$999/yr',
-        disc: '12Months at $50/Month'
-    })
+  const selectedImage = require("../../assets/Images/selectedIcon.png");
+  const unSelectedImage = require("../../assets/Images/unSelectedIcon.png");
 
-    const selectedImage = require('../../assets/Images/selectedIcon.png')
-    const unSelectedImage = require('../../assets/Images/unSelectedIcon.png')
+  const plans = [
+    {
+      id: 1,
+      name: "YEARLY",
+      price: "$999/yr",
+      disc: "12 Months at $50/Month",
+    },
+    {
+      id: 2,
+      name: "MONTHLY",
+      price: "$999/m",
+      disc: "",
+    },
+  ];
 
-    const plans = [
-        {
-            id: 1,
-            name: "YEARLY",
-            price: '$999/yr',
-            disc: '12Months at $50/Month'
-        },
-        {
-            id: 2,
-            name: "MONTHLY",
-            price: '$999/m',
-            disc: ''
-        }
-    ]
+  return (
+    <SafeAreaView style={GlobalStyles.container}>
+      <View style={[GlobalStyles.container, { alignItems: "center" }]}>
+        <View
+          style={{
+            flexDirection: "column",
+            alignItems: "center",
+            height: screenHeight - 100,
+            justifyContent: "flex-end",
+          }}
+        >
+          <Text style={GlobalStyles.heading}>Subscription Plan</Text>
+          <Text
+            style={[
+              GlobalStyles.subheading14,
+              { marginTop: 10, marginBottom: (50 / 930) * screenHeight },
+            ]}
+          >
+            Lorem ipsum dor simet insnet
+          </Text>
 
-    return (
-        <SafeAreaView style={GlobalStyles.container}>
-            <View style={[GlobalStyles.container, { alignItems: 'center', }]}>
-                <View style={{ flexDirection: 'column', alignItems: 'center', height: screenHeight - 100, justifyContent: 'flex-end' }}>
-                    <Text style={GlobalStyles.heading}>
-                        Subscription Plan
-                    </Text>
-                    <Text style={[GlobalStyles.subheading14, { marginTop: 10,marginBottom:50/930*screenHeight }]}>
-                        Lorem ipsum dor simet insnet
-                    </Text>
-
-                    {
-                        plans.map((item) => (
-                            <TouchableOpacity key={item.id} 
-                                onPress={() => {
-                                    setselectedPlan(item)
-                                }}
-                            >
-                                {
-                                    item.id === 1 && (
-                                        <Image source={require('../../assets/Images/recommendedImage.png')}
-                                            style={{
-                                                height: 30 / 930 * screenHeight, width: 164 / 430 * screenWidth, resizeMode: 'contain',
-                                                zIndex: 2,alignSelf:'flex-end',marginRight:30/430*screenWidth,marginBottom:-45/930*screenHeight,
-                                                // position: 'relative', top: 0, bottom: 50, 
-                                            }}
-                                        />
-                                    )
-                                }
-                                <View style={{
-                                    borderWidth: 1, borderColor: selectedPlan.id === item.id ? Colors.orangeColor : Colors.grayColor,
-                                    borderRadius: 5, alignItems: 'center',
-                                    paddingVertical: 15, paddingHorizontal: 10,marginTop: 30 / 930 * screenHeight,
-                                     zIndex: 1
-                                }}>
-                                    <View style={{ width: screenWidth - 80 / 430 * screenWidth, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-                                        <View style={{ flexDirection: 'column', alignItems: 'center', gap: 15 }}>
-                                            <Text style={{
-                                                fontSize: 17, fontFamily: CustomFonts.PoppinsSemiBold, fontWeight: '700',
-                                                color: Colors.orangeColor
-                                            }}>
-                                                {item.name}
-                                            </Text>
-
-                                            <Text style={{
-                                                fontSize: 17, fontFamily: CustomFonts.PoppinsSemiBold, fontWeight: '700',
-                                                color: "black"
-                                            }}>
-                                                {item.price}
-                                            </Text>
-                                        </View>
-
-
-                                        <View style={{ flexDirection: 'column', alignItems: 'flex-end', gap: 15, justifyContent: 'center' }}>
-
-                                            <Image source={selectedPlan.id === item.id ? selectedImage : unSelectedImage}
-                                                style={GlobalStyles.image24}
-                                            />
-                                            <Text style={{ fontSize: 17, fontFamily: CustomFonts.InterRegular, }}>
-                                                {item.disc}
-                                            </Text>
-                                        </View>
-                                    </View>
-                                </View>
-                            </TouchableOpacity>
-
-                        ))
-                    }
-
-                    <TouchableOpacity style={GlobalStyles.capsuleBtn}
-                        onPress={()=>{
-                            navigation.push(ScreenNames.ProfileCongratsScreen)
-                        }}
+          {plans.map((item) => (
+            <TouchableOpacity
+              key={item.id}
+              onPress={() => {
+                setselectedPlan(item);
+              }}
+            >
+              {item.id === 1 && (
+                <Image
+                  source={require("../../assets/Images/recommendedImage.png")}
+                  style={{
+                    height: (30 / 930) * screenHeight,
+                    width: (164 / 430) * screenWidth,
+                    resizeMode: "contain",
+                    zIndex: 2,
+                    alignSelf: "flex-end",
+                    marginRight: (30 / 430) * screenWidth,
+                    marginBottom: (-45 / 930) * screenHeight,
+                    // position: 'relative', top: 0, bottom: 50,
+                  }}
+                />
+              )}
+              <View
+                style={{
+                  borderWidth: 1,
+                  borderColor:
+                    selectedPlan.id === item.id
+                      ? Colors.orangeColor
+                      : Colors.grayColor,
+                  borderRadius: 5,
+                  alignItems: "center",
+                  paddingVertical: 15,
+                  paddingHorizontal: 10,
+                  marginTop: (30 / 930) * screenHeight,
+                  zIndex: 1,
+                }}
+              >
+                <View
+                  style={{
+                    width: screenWidth - (80 / 430) * screenWidth,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <View
+                    style={{
+                      flexDirection: "column",
+                      alignItems: "center",
+                      gap: 15,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        fontSize: 17,
+                        fontFamily: CustomFonts.PoppinsSemiBold,
+                        fontWeight: "700",
+                        color: Colors.orangeColor,
+                      }}
                     >
-                        <Text style={GlobalStyles.BtnText}>
-                            Subscribe
-                        </Text>
-                    </TouchableOpacity>
-
-                    <Text style={{
-                        width: screenWidth - 40, fontSize: 13, fontFamily: CustomFonts.InterRegular,
-                        marginTop: 30, textAlign: 'center'
-                    }}>
-                        This subscription automically renews unless auto-renew is turn off ateast 24hrs before current period eands. Payment is charged to your iTunes Account.
+                      {item.name}
                     </Text>
+
+                    <Text
+                      style={{
+                        fontSize: 17,
+                        fontFamily: CustomFonts.PoppinsSemiBold,
+                        fontWeight: "700",
+                        color: "black",
+                      }}
+                    >
+                      {item.price}
+                    </Text>
+                  </View>
+
+                  <View
+                    style={{
+                      flexDirection: "column",
+                      alignItems: "flex-end",
+                      gap: 15,
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Image
+                      source={
+                        selectedPlan.id === item.id
+                          ? selectedImage
+                          : unSelectedImage
+                      }
+                      style={GlobalStyles.image24}
+                    />
+                    <Text
+                      style={{
+                        fontSize: 17,
+                        fontFamily: CustomFonts.InterRegular,
+                      }}
+                    >
+                      {item.disc}
+                    </Text>
+                  </View>
                 </View>
+              </View>
+            </TouchableOpacity>
+          ))}
 
-            </View>
-        </SafeAreaView>
-    )
-}
+          <TouchableOpacity
+            style={GlobalStyles.capsuleBtn}
+            onPress={() => {
+              navigation.push(ScreenNames.ProfileCongratsScreen);
+            }}
+          >
+            <Text style={GlobalStyles.BtnText}>Subscribe</Text>
+          </TouchableOpacity>
 
-export default PlansScreen
+          <Text
+            style={{
+              width: screenWidth - 40,
+              fontSize: 13,
+              fontFamily: CustomFonts.InterRegular,
+              marginTop: 30,
+              textAlign: "center",
+            }}
+          >
+            This subscription automically renews unless auto-renew is turn off
+            ateast 24hrs before current period eands. Payment is charged to your
+            iTunes Account.
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default PlansScreen;
